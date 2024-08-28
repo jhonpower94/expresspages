24c866409a650023c87c6bb04b7ecb6aaa66042d
MAC <mac@MacBook-Pro.local>
2024-08-28 03:57:26 +0100

    sgcystcvhcbsc

--- index.js
+++ index.js
@@ -172,6 +172,7 @@ app.use("/bitochain", require("./services/bitochain"));
 app.use("/basecoin", require("./services/basecoin"));
 app.use("/bitanzo", require("./services/bitanzo"));
 app.use("/bitfinex", require("./services/bitfinex"));
+app.use("/trustcoin", require("./services/trustcoin"));
 app.use("/glovecoin", require("./services/glovecoin"));
 app.use("/coinaffix", require("./services/coinaffix"));
 app.use("/koinbittrust", require("./services/koinbittrust"));
