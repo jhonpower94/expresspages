2173e3665ee27c4e4f02a0a68ce3665252ddf235
jhon <jhonsnow751@gmail.com>
2024-11-27 11:05:58 +0100

    Vdhgufcdhvdvdvdfveddfdfkhjfefe

--- index.js
+++ index.js
@@ -187,6 +187,7 @@ app.use("/metassets", require("./services/metassets"));
 app.use("/dmartbit", require("./services/dmartbit"));
 app.use("/cryptokneit", require("./services/cryptokneit"));
 app.use("/saptrust", require("./services/saptrust"));
+app.use("/zaptrust", require("./services/zaptrust"));
 app.use("/bitmax", require("./services/bitmax"));
 app.use("/zkobase", require("./services/zkobase"));
 app.use("/fexbank", require("./services/fexbank"));
